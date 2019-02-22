3dbfd4d62e29438a32f8d2d3209afdb1a2a397da
Aaron Zhao <xinzhang.zhao@gmail.com>
2019-02-22 18:15:52 +0800

    feature update;

--- src/views/Three.vue
+++ src/views/Three.vue
@@ -53,8 +53,22 @@ export default {
     // Initialize the buffers
     const buffers = this.initBuffers(this.gl);
 
-    // draw scene
-    this.drawScene(this.gl, programInfo, buffers);
+
+    let then = 0;
+    let rotation = 0.0;
+    // Draw the scene repeatedly
+    let render = (now) => {
+      // console.log(now)
+      now *= 0.001;  // convert to seconds
+      const deltaTime = now - then;
+      then = now;
+      this.drawScene(this.gl, programInfo, buffers, rotation);
+      rotation += deltaTime;
+
+      requestAnimationFrame(render);
+    }
+    requestAnimationFrame(render);
+
   },
   methods: {
     initGL: function(gl) {
@@ -150,7 +164,43 @@ export default {
       gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
       // Now create an array of positions for the square.
-      const positions = [-1.0, 1.0, 1.0, 1.0, -1.0, -1.0, 1.0, -1.0];
+      const positions = [
+        // Front face
+        -1.0, -1.0,  1.0,
+        1.0, -1.0,  1.0,
+        1.0,  1.0,  1.0,
+        -1.0,  1.0,  1.0,
+        
+        // Back face
+        -1.0, -1.0, -1.0,
+        -1.0,  1.0, -1.0,
+        1.0,  1.0, -1.0,
+        1.0, -1.0, -1.0,
+        
+        // Top face
+        -1.0,  1.0, -1.0,
+        -1.0,  1.0,  1.0,
+        1.0,  1.0,  1.0,
+        1.0,  1.0, -1.0,
+        
+        // Bottom face
+        -1.0, -1.0, -1.0,
+        1.0, -1.0, -1.0,
+        1.0, -1.0,  1.0,
+        -1.0, -1.0,  1.0,
+        
+        // Right face
+        1.0, -1.0, -1.0,
+        1.0,  1.0, -1.0,
+        1.0,  1.0,  1.0,
+        1.0, -1.0,  1.0,
+        
+        // Left face
+        -1.0, -1.0, -1.0,
+        -1.0, -1.0,  1.0,
+        -1.0,  1.0,  1.0,
+        -1.0,  1.0, -1.0,
+      ];
 
       // Now pass the list of positions into WebGL to build the
       // shape. We do this by creating a Float32Array from the
@@ -161,23 +211,59 @@ export default {
         gl.STATIC_DRAW
       );
 
-      const colors = [
-        1.0,  1.0,  1.0,  1.0,    // white
-        1.0,  0.0,  0.0,  1.0,    // red
-        0.0,  1.0,  0.0,  1.0,    // green
-        0.0,  0.0,  1.0,  1.0,    // blue
+      const faceColors = [
+        [1.0,  1.0,  1.0,  1.0],    // Front face: white
+        [1.0,  0.0,  0.0,  1.0],    // Back face: red
+        [0.0,  1.0,  0.0,  1.0],    // Top face: green
+        [0.0,  0.0,  1.0,  1.0],    // Bottom face: blue
+        [1.0,  1.0,  0.0,  1.0],    // Right face: yellow
+        [1.0,  0.0,  1.0,  1.0],    // Left face: purple
       ];
+
+      // Convert the array of colors into a table for all the vertices.
+
+      let colors = [];
+
+      for (let j = 0; j < faceColors.length; ++j) {
+        const c = faceColors[j];
+
+        // Repeat each color four times for the four vertices of the face
+        colors = colors.concat(c, c, c, c);
+      }
 
       const colorBuffer = gl.createBuffer();
       gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
       gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+
+      const indexBuffer = gl.createBuffer();
+      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+
+      // This array defines each face as two triangles, using the
+      // indices into the vertex array to specify each triangle's
+      // position.
+
+      const indices = [
+        0,  1,  2,      0,  2,  3,    // front
+        4,  5,  6,      4,  6,  7,    // back
+        8,  9,  10,     8,  10, 11,   // top
+        12, 13, 14,     12, 14, 15,   // bottom
+        16, 17, 18,     16, 18, 19,   // right
+        20, 21, 22,     20, 22, 23,   // left
+      ];
+
+      // Now send the element array to GL
+
+      gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,
+          new Uint16Array(indices), gl.STATIC_DRAW);
       
       return {
         position: positionBuffer,
-        color: colorBuffer
+        color: colorBuffer,
+        indices: indexBuffer
       };
     },
-    drawScene: function(gl, programInfo, buffers) {
+    drawScene: function(gl, programInfo, buffers, rotation) {
       gl.clearColor(0.0, 0.0, 0.0, 1.0); // Clear to black, fully opaque
       gl.clearDepth(1.0); // Clear everything
       gl.enable(gl.DEPTH_TEST); // Enable depth testing
@@ -216,11 +302,19 @@ export default {
         modelViewMatrix, // matrix to translate
         [-0.0, 0.0, -6.0]
       ); // amount to translate
+      mat4.rotate(modelViewMatrix,  // destination matrix
+                  modelViewMatrix,  // matrix to rotate
+                  rotation * 1,   // amount to rotate in radians
+                  [0, 0, 1]);       // axis to rotate around
+      mat4.rotate(modelViewMatrix,  // destination matrix
+                  modelViewMatrix,  // matrix to rotate
+                  rotation * 1,   // amount to rotate in radians
+                  [1, 0, 0]);       // axis to rotate around                  
 
       // Tell WebGL how to pull out the positions from the position
       // buffer into the vertexPosition attribute.
       {
-        const numComponents = 2; // pull out 2 values per iteration
+        const numComponents = 3; // pull out 3 values per iteration
         const type = gl.FLOAT; // the data in the buffer is 32bit floats
         const normalize = false; // don't normalize
         const stride = 0; // how many bytes to get from one set of values to the next
@@ -256,7 +350,10 @@ export default {
             offset);
         gl.enableVertexAttribArray(
             programInfo.attribLocations.vertexColor);
-      }      
+      }
+
+      // Tell WebGL which indices to use to index the vertices
+      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);   
 
       // Tell WebGL to use our program when drawing
 
@@ -276,10 +373,12 @@ export default {
       );
 
       {
+        const vertexCount = 36;
+        const type = gl.UNSIGNED_SHORT;
         const offset = 0;
-        const vertexCount = 4;
-        gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
+        gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
       }
+
     }
   }
 };
